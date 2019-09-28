7de0e2dd21432c36eb019de2c01245569fa110dd
Gregor Reitzenstein <me@dequbed.space>
2019-09-28 10:30:52 +0200

    Remove old Metadata structs

--- lib/rarian/src/storage.rs
+++ lib/rarian/src/storage.rs
@@ -4,33 +4,6 @@ use serde::{Serialize, Deserialize};
 
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Book {
-    pub filename: String,
-    pub author: Option<String>,
-    pub title: Option<String>,
-    pub subject: Option<String>,
-    pub description: Option<String>,
-    pub date: Option<String>,
-    pub identifier: Option<String>,
-    pub language: Option<String>,
-    pub publisher: Option<String>,
-    pub license: Option<String>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Song {
-    pub artist: Vec<String>,
-    pub title: String,
-    pub album: Option<String>,
-    pub genre: Option<String>,
-    pub track: Option<u32>,
-    pub totaltracks: Option<u32>,
-    pub albumartist: Option<String>,
-    pub lyrics: Option<String>,
-}
-
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Storables {
     Text(Book),
     Audio(Song),
